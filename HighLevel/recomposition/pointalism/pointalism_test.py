3939db127a48e6d46eefba2927cb028a3f354bcc
Odell Dotson <odelldotson@gmail.com>
2016-12-02 15:02:33 -0500

    Factored out recompose inherritance from pointalism

--- HighLevel/recomposition/pointalism/pointalism_test.py
+++ HighLevel/recomposition/pointalism/pointalism_test.py
@@ -1,7 +1,7 @@
 import pointalismRecompose as rc
 import cv2
 
-desiredImg = cv2.imread('cat.png', cv2.IMREAD_UNCHANGED)
+desiredImg = cv2.imread('../../resources/images/input/cat.png', cv2.IMREAD_UNCHANGED)
 
 recomposer = rc.pointalismRecomposer(desiredImg, [10])
 recomposer.recompose()
